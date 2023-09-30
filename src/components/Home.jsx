97cb68b6ce19f07f9914c2ce515fa81b0f4a54ad
Wentana Ghile <wentanaghile@Wentana-MacBook-Pro.local>
2023-09-30 11:44:04 -0400

    styled home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,25 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div className='Home'>
-        Home
+        <h1>
+          Welcome
+        </h1>
+        <div id='text'>
+          <p id='greeting'>
+            Hi, I'm Wentana <span id='flag'>🇪🇷</span>
+          </p>
+          <p id='goals'>
+            Full-Stack Software Engineer in the Making
+          </p>
+        </div>
+        <div id='links'>
+          <a href='/about'>
+            More About Me
+          </a>
+          <a href='/portfolio'>
+            View Projects
+          </a>
+        </div>
     </div>
   )
 }
